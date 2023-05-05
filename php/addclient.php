fa4f1a3c4911173bacaed6b005432f6147c3ad89
Maryam CONSANI <124218131+Korotsuki@users.noreply.github.com>
2023-05-05 22:38:28 +0200

    Update addclient.php

--- php/addclient.php
+++ php/addclient.php
@@ -6,13 +6,12 @@ include('connexion_inc.php');
 // Requête SQL pour insérer dans d_client
 $cnx->exec("BEGIN;"); //Début transaction
 $req=$cnx->prepare("INSERT INTO d_client VALUES (DEFAULT,?,?,?,?)"); //CodeCli, Nom, Prenom, Adresse,Telephone
-$req->execute(array($_POST['nom'],$_POST['prenom'],$_POST['adresse'],$_POST['num'])); // Remplace ? par les valeurs entrées sauf pour le codecli = Default
-$code_client = (int) ($conn->lastInsertId()) +2;
+$req->execute(array($_POST['nom'],$_POST['prenom'],$_POST['adresse'],$_POST['num'])); // Remplace ? par les valeurs entrées
 
 
 // Requête SQL pour insérer dans d_login
-$req2=$cnx->prepare("INSERT INTO d_login VALUES (?,?,?)"); //ID, Mot de passe, Codecli
-$req2->execute(array($_POST['id'],md5($_POST['mdp']),$codecli)); // Remplace ? par les valeurs entrées
+$req2=$cnx->prepare("INSERT INTO d_login VALUES (?,?,DEFAULT)"); //ID, Mot de passe, Codecli
+$req2->execute(array($_POST['id'],$_POST['mdp'])); // Remplace ? par les valeurs entrées
 
 $cnx->exec("COMMIT;"); //Fin transaction
 
