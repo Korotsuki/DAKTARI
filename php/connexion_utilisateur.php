2df7daca1ed22575d543b5ffd86c0864940932e7
Héléna Chevalier <115875870+Artena8@users.noreply.github.com>
2023-05-06 12:49:24 +0200

    Add files via upload

--- php/connexion_utilisateur.php
+++ php/connexion_utilisateur.php
@@ -4,7 +4,7 @@ include('connexion_inc.php');
 session_start();
 
 function getClientCode($id, $mdp, $cnx){
-    $stmt = $cnx->prepare('SELECT codecli FROM d_login WHERE id = ? AND mdp = ?;');
+    $stmt = $cnx->prepare("SELECT codecli FROM d_login WHERE id = ? AND mdp = ? AND acces='CLIENT';");
     $stmt->execute([$id, $mdp]);
     $ligne = $stmt->fetch();
 
@@ -15,7 +15,7 @@ function getClientCode($id, $mdp, $cnx){
 }
 
 function getVetoCode($id, $mdp, $cnx){
-    $stmt = $cnx->prepare("SELECT * FROM d_login WHERE id = ? AND mdp = ? AND acces='ADMIN';");
+    $stmt = $cnx->prepare("SELECT * FROM d_login WHERE id = ? AND mdp = ? AND acces='VETO';");
     $stmt->execute([$id,$mdp]);
     $ligne = $stmt->fetch();
 
@@ -25,23 +25,44 @@ function getVetoCode($id, $mdp, $cnx){
     return false;
 }
 
+if (!function_exists('getAdminCode')) {
+    function getAdminCode($id, $mdp, $cnx){
+        $stmt = $cnx->prepare("SELECT codecli FROM d_login WHERE id = ? AND mdp = ? AND acces='ADMIN';");
+        $stmt->execute([$id, $mdp]);
+        $ligne = $stmt->fetch();
+
+        if ($ligne) {
+            return $ligne['id'];
+        }
+        return false;
+    }
+}
+
 if (isset($_POST['id']) && isset($_POST['mdp'])){
 
     // si c'est un admin
-    if ($_POST['id']=='admin' && $_POST['mdp']=='admin'){
+    if ($_POST['id'] == 'admin' && $_POST['mdp'] == 'admin'){
         $_SESSION['id'] = $_POST['id'];
         $_SESSION['mdp'] = $_POST['mdp'];
+        $_SESSION['acces'] = "ADMIN";
 
-        header('Location: ../admin.php');
+        header('Location: ../droitspages.php');
         exit;
-        
+
+    }elseif (getAdminCode($_POST['id'], $_POST['mdp'], $cnx) !== false){
+        $_SESSION['id'] = $_POST['id'];
+        $_SESSION['mdp'] = $_POST['mdp'];
+        $_SESSION['acces'] = "ADMIN";
+
+        header('Location: ../droitspages.php');
+        exit;
     }elseif (getVetoCode( $_POST['id'], $_POST['mdp'],$cnx) != false){// c'est le véto...
         
         $_SESSION['id'] = $_POST['id'];
         $_SESSION['mdp'] = $_POST['mdp'];
-        $_SESSION['id'] = getVetoCode( $_POST['id'], $_POST['mdp'],$cnx);
+        $_SESSION['acces'] = "VETO";
 
-        header('Location: ../veto.php');
+        header('Location: ../dossiersclients.php');
         exit;
 
     }elseif (getClientCode( $_POST['id'], $_POST['mdp'],$cnx) != false){// c'est un client...
@@ -49,8 +70,9 @@ if (isset($_POST['id']) && isset($_POST['mdp'])){
         $_SESSION['id'] = $_POST['id'];
         $_SESSION['mdp'] = $_POST['mdp'];
         $_SESSION['codecli'] = getClientCode( $_POST['id'], $_POST['mdp'],$cnx);
-
-        header('Location: ../myaccount.php');
+        $_SESSION['acces'] = "CLIENT";
+        
+        header('Location: ../moncompte.php');
         exit;
 
     }else{// sinon (erreur)... -> Il n'y a pas de compte correspondand=
